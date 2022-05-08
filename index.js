bb5942972e449afb507211c7193e933b470283d6
desislava-deneva <dessy8905@gmail.com>
2022-05-08 17:49:38 +0300

    add database

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // [x] create home controler
 // [x] bind routing 
 // [x] create layout
+
 //  create data service
 //--[x] read all
 //--[x] read one by id
@@ -10,6 +11,9 @@
 //--[x] edit
 //--[x] delete
 //--[x] search
+//--[] accessory read
+//--[] accessory create
+//--[] attach accessory
 
 // implement controllers
 //--[x] home (catalog)
@@ -19,55 +23,72 @@
 //--[x] improved home search
 //--[x] edit 
 //--[x] delete 
+//--[] create accsssory
+//--[] attach accsesoty to car
+//--[] upgrade details to include accessory
+//--[]
 
 // [x] add frond-end code 
+//[] add database connection
+//[] create car model
+//[] upgrade car servise to use Car model
+//[] create Accsessory models
+//[] 
 
 const express = require('express')
 const hbs = require(`express-handlebars`);
+
+const initDb = require('./models/index');
+
 const carService = require('./services/cars')
 
-const { notFound } = require('./controlers/404');
+const { home } = require('./controlers/home');
 const { about } = require('./controlers/about');
 const create = require('./controlers/create');
 const { details } = require('./controlers/details');
-const { home } = require('./controlers/home');
-const deleteCar = require('./controlers/delete');
 const editCar = require('./controlers/edit');
+const deleteCar = require('./controlers/delete');
+
+const { notFound } = require('./controlers/404');
+start()
+
+async function start() {
+    await initDb()
+
+    const app = express();
+
+    app.engine('hbs', hbs.create({
+        extname: '.hbs'
+    }).engine)
+
+    app.set('view engine', '.hbs')
+
+    app.use(express.urlencoded({ extended: true }));
+    app.use('/static', express.static('static'));
+    app.use(carService())
+
+    app.get('/', home)
+    app.get('/about', about);
+    app.get('/details/:id', details);
+
+    app.route('/create')
+        .get(create.get)
+        .post(create.post)
+
+    app.route('/delete/:id')
+        .get(deleteCar.get)
+        .post(deleteCar.post)
+
+    app.route('/edit/:id')
+        .get(editCar.get)
+        .post(editCar.post)
 
 
-const app = express();
-
-app.engine('hbs', hbs.create({
-    extname: '.hbs'
-}).engine)
-
-app.set('view engine', '.hbs')
-
-app.use(express.urlencoded({ extended: true }));
-app.use('/static', express.static('static'));
-app.use(carService())
-
-app.get('/', home)
-app.get('/about', about);
-app.get('/details/:id', details);
-
-app.route('/create')
-    .get(create.get)
-    .post(create.post)
-
-app.route('/delete/:id')
-    .get(deleteCar.get)
-    .post(deleteCar.post)
-
-app.route('/edit/:id')
-    .get(editCar.get)
-    .post(editCar.post)
+    // app.get('/create', create.get);
+    // app.get('/create', create.post);
 
 
-// app.get('/create', create.get);
-// app.get('/create', create.post);
+    app.all('*', notFound)
 
-
-app.all('*', notFound)
-
-app.listen(3000, () => console.log('Server strt post 3000'))
+    app.listen(3000, () => console.log('Server strt post 3000'))
+}
---
+++ models/index.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Car = require('./Car');
+
+const conectionString = 'mongodb://localhost:27017/carbicle';
+
+async function init() {
+    try {
+        await mongoose.connect(conectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log('Database condected');
+
+        mongoose.connection.on('error', (error) => {
+            console.error('database error');
+            console.error(error);
+        });
+        
+    } catch (error) {
+        console.error('Error conecting to database');
+        process.exit(1);
+    }
+
+}
+
+module.exports = init;
